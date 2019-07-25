3538f6a3aec873abd62dac31fbe722792d3b030d
anant <anant@anant.com>
2019-07-25 16:00:18 +0530

    Added yeehaw.cpp with comments on what their actual code looks like

---
+++ yeehaw.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#define Yee ;
+#define Haw main()
+#define yEe {
+#define hAw }
+#define yeE int
+#define haW using
+#define YEe namespace
+#define HAw std
+#define YeE while
+#define HaW (
+#define yEE )
+#define hAW =
+#define YEE 1
+#define HAW O
+#define yeee cout
+#define haaw <<
+#define yeeehaaw cin
+#define Yeee >>
+#define Haaw +
+#define YEEE endl
+#define yee ==
+
+haW YEe HAw Yee
+//using std namespace ;
+
+yeE Haw yEe
+// int main() {
+    yeE yeeHaw Yee
+    //int yeeHaw ;
+    yeE YeeHaw Yee
+    //int YeeHaw ;
+    yeE yeehaw hAW YEE Yee
+    //int yeehaw = 1;
+
+    YeE HaW yeehaw yee 1 yEE yEe
+    // while (yeehaw == 1 ) ;
+        yeee haaw "Yee haw 2 yee haws: " Yee
+        // cout << "Yee haw 2 yee haws: " ;
+        yeeehaaw Yeee yeeHaw Yeee YeeHaw Yee
+        // cin >> yeeHaw >> YeeHaw ;
+        yeee haaw yeeHaw Haaw YeeHaw haaw YEEE Yee
+        // cout << yeeHaw + YeeHaw << endl ;
+        yeee haaw "YeeHaw again? " Yee
+        // cout << "YeeHaw again? " ;
+        yeeehaaw Yeee yeehaw Yee
+        // cin >> yeehaw;
+    hAw
+    // }
+hAw
+// }
